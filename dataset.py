ad3a7411c2cca37d7145ae28b59fa624621e4fd3
Yuri Stepin <enchantinggg4@gmail.com>
2022-08-24 22:16:44 +0300

    transform dataset

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 
+import argparse
 from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
 import torchvision.transforms as T
@@ -7,6 +8,9 @@ from PIL import Image
 from skimage import io, transform, color
 import torch
 from tqdm import tqdm
+from pathlib import Path
+from torchvision.utils import save_image
+
 class UpsampleDataset(Dataset):
     def __init__(self, path, i_image_size, o_image_size):
         self.root_dir = path
@@ -20,6 +24,19 @@ class UpsampleDataset(Dataset):
         self.y = []
 
         print(f'Got {len(self.images)} images')
+
+    def transform_dataset(self):
+        i_transform = T.Resize((self.i_image_size, self.i_image_size))
+        o_transform = T.Resize((self.o_image_size, self.o_image_size))
+        to_tensor = T.ToTensor()
+        for idx, img_name in tqdm(enumerate(self.images)):
+            image = Image.open(img_name).convert('RGB')
+            i_image = i_transform(image)
+            o_image = o_transform(image)
+
+            i_image.save(f'dataset/x/{idx}.jpg')
+            o_image.save(f'dataset/y/{idx}.jpg')
+
 
 
     def gpu_precache(self, device):
@@ -37,14 +54,14 @@ class UpsampleDataset(Dataset):
             # if image.shape[2] > 3:
             #     image = color.rgba2rgb(image)
 
-            i_image = to_tensor(i_transform(image))
-            o_image = to_tensor(o_transform(image))
+            i_image = i_transform(to_tensor(image))
+            o_image = o_transform(to_tensor(image))
 
             self.x.append(i_image)
             self.y.append(o_image)
 
-        self.x = torch.stack(self.x).to(device)
-        self.y = torch.stack(self.y).to(device)
+        self.x = torch.stack(self.x)#.to(device)
+        self.y = torch.stack(self.y)#.to(device)
         print('Device precache complete')
         
     def __len__(self):
@@ -52,3 +69,18 @@ class UpsampleDataset(Dataset):
     
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Train model')
+    parser.add_argument('-p', action='store', dest='path')
+
+    args = parser.parse_args()
+
+
+    # transform all images to cropped
+    Path('./dataset/x').mkdir(parents=True, exist_ok=True)
+    Path('./dataset/y').mkdir(parents=True, exist_ok=True)
+    
+    UpsampleDataset(args.path, 64, 100).transform_dataset()
