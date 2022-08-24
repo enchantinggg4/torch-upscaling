7729409cb4072e4a18f330bf005ba5a50b27efce
Yuri Stepin <enchantinggg4@gmail.com>
2022-08-24 20:49:12 +0300

    test

--- dataset.py
+++ dataset.py
@@ -23,7 +23,7 @@ class UpsampleDataset(Dataset):
     def gpu_precache(self, device):
         for idx, img_name in tqdm(enumerate(self.images)):
 
-            if idx > 50:
+            if idx > 2000:
                 break
         
             image = io.imread(img_name)
