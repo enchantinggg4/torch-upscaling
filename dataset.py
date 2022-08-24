96d0dd2cae3422a0c7a29b1797f5007deb113045
Yuri Stepin <enchantinggg4@gmail.com>
2022-08-24 22:34:17 +0300

    no ram

--- dataset.py
+++ dataset.py
@@ -39,36 +39,46 @@ class UpsampleDataset(Dataset):
 
 
     def gpu_precache(self, device):
+        pass
 
         x_images = [os.path.join(dp, f) for dp, dn, filenames in os.walk(os.path.join(self.root_dir, 'x')) for f in filenames if os.path.splitext(f)[1] in ['.png', '.jpg', '.jpeg']]
         y_images = [os.path.join(dp, f) for dp, dn, filenames in os.walk(os.path.join(self.root_dir, 'y')) for f in filenames if os.path.splitext(f)[1] in ['.png', '.jpg', '.jpeg']]
 
         self.len = min(len(x_images), len(y_images))
         print(f'Got {self.len} images')
-        i_transform = T.Resize((self.i_image_size, self.i_image_size))
-        o_transform = T.Resize((self.o_image_size, self.o_image_size))
-        to_tensor = T.ToTensor()
+        # i_transform = T.Resize((self.i_image_size, self.i_image_size))
+        # o_transform = T.Resize((self.o_image_size, self.o_image_size))
+        # to_tensor = T.ToTensor()
 
-        for idx in tqdm(range(self.len)):
-            i_image = Image.open(os.path.join(self.root_dir, 'x', f'{idx}.jpg')).convert('RGB')
-            o_image = Image.open(os.path.join(self.root_dir, 'x', f'{idx}.jpg')).convert('RGB')
+        # for idx in tqdm(range(self.len)):
+        #     i_image = Image.open(os.path.join(self.root_dir, 'x', f'{idx}.jpg')).convert('RGB')
+        #     o_image = Image.open(os.path.join(self.root_dir, 'x', f'{idx}.jpg')).convert('RGB')
 
 
-            i_image = i_transform(to_tensor(i_image))
-            o_image = o_transform(to_tensor(o_image))
+        #     i_image = i_transform(to_tensor(i_image))
+        #     o_image = o_transform(to_tensor(o_image))
 
-            self.x.append(i_image)
-            self.y.append(o_image)
+        #     self.x.append(i_image)
+        #     self.y.append(o_image)
 
-        self.x = torch.stack(self.x).to(device)
-        self.y = torch.stack(self.y).to(device)
-        print('Device precache complete')
+        # self.x = torch.stack(self.x).to(device)
+        # self.y = torch.stack(self.y).to(device)
+        # print('Device precache complete')
         
+
     def __len__(self):
         return self.len
     
     def __getitem__(self, idx):
-        return self.x[idx], self.y[idx]
+        i_transform = T.Resize((self.i_image_size, self.i_image_size))
+        o_transform = T.Resize((self.o_image_size, self.o_image_size))
+        to_tensor = T.ToTensor()
+        i_image = Image.open(os.path.join(self.root_dir, 'x', f'{idx}.jpg')).convert('RGB')
+        o_image = Image.open(os.path.join(self.root_dir, 'x', f'{idx}.jpg')).convert('RGB')
+
+        i_image = i_transform(to_tensor(i_image))
+        o_image = o_transform(to_tensor(o_image))
+        return i_image, o_image
 
 
 if __name__ == "__main__":
